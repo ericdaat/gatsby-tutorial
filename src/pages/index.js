f33ddc8c9ac70df1b230c8c6b29a31866eac1139
Eric Daoud <daoudattoyan.eric@gmail.com>
2019-09-20 15:22:17 +0200

    completed part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
-import Header from "../components/header"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default () => (
-    <div>
-        <Link to="/contact/">Contact</Link>
-        <Header headerText="About Gatsby"/>
-        <h1>Hello Gatsby!</h1>
-        <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+  <Layout>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
 )
-
